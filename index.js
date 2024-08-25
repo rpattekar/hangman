1502a0e2ee14c5c9d798064be19c305f5ac55dfe
rpattekar <rpattekar@wisc.edu>
2024-08-25 02:16:15 -0700

    Added all hangman body parts.
    Co-Authored-By: Yash Pattekar <109927564+rokkc@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,14 @@
+const head = document.getElementById("head");
+const torso = document.getElementById("torso");
+const arm1 = document.getElementById("arm1");
+const arm2 = document.getElementById("arm2");
+const leg1 = document.getElementById("leg1");
+const leg2 = document.getElementById("leg2");
+
+console.log(head);
+console.log(torso);
+console.log(arm1);
+console.log(arm2);
+console.log(leg1);
+console.log(leg2);
+
